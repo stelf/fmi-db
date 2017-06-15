edc8db4d7fd1fff8406f066f8989d2f3ba0d34d1
Gheorghi <larodi@gmail.com>
2017-06-15 16:45:26 +0300

    more solution

--- examples/adventureworks/latest-hired-employee.sql
+++ examples/adventureworks/latest-hired-employee.sql
@@ -1,3 +1,8 @@
+/* Task: Find the most recently hired employees */
+
+
+/* variant 1 */
+
 SELECT
 	p.FirstName,
 	p.LastName,
@@ -8,4 +13,28 @@ inner join
 	Person.Person p on
 	e.BusinessEntityID = p.BusinessEntityID
 WHERE
-	e.HireDate >= ALL (SELECT HireDate from HumanResources.Employee)
+	e.HireDate >= ALL (
+		SELECT
+			HireDate 
+		FROM 
+			HumanResources.Employee)
+
+/* variant 2 */
+
+SELECT 
+	Person.FirstName, 
+	Person.LastName
+FROM
+	PERSON.Person 
+WHERE 
+	BusinessEntityID in (
+	SELECT
+		BusinessEntityId 
+	FROM
+		HumanResources.Employee
+	WHERE
+		HireDate = (
+			SELECT MAX(HireDate) 
+			From HumanResources.Employee
+		)
+	)
