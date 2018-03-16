7fed46ba33713de4dc36f6a8d1cdd029fa630639
Gheorghi <larodi@gmail.com>
2018-03-16 17:16:01 +0200

    Update total.artists.sales.sql
    postgresql version

--- examples/chinook/total.artists.sales.sql
+++ examples/chinook/total.artists.sales.sql
@@ -2,6 +2,8 @@
 /* парчета на каква стойност е качил в базата всеки артист (без компилации)? */
 /* total value of tracks uploaded by certain artists (not including single tracks in 'Various Artists' compilations) */
 
+/* SQLITE version */
+
 SELECT   ar.name as "artistname",
          al.title as "albumname",
          al.albumid,
@@ -18,4 +20,24 @@ WHERE    ar.artistid != (
           where arin.name = 'Various Artists')
 GROUP BY ar.artistid, al.albumid
 ORDER by ar.artistid
+
+/* PostgreSQL Version */
+
+
+SELECT   ar."Name" as "artistname",
+         al."Title" as "albumname",
+         al."AlbumId",
+         ar."ArtistId",
+         sum("UnitPrice") as "total amount"
+FROM     "Album" as al
+JOIN     "Artist" as ar
+ON       al."ArtistId" = ar."ArtistId"
+JOIN     "Track" as tr
+ON       al."AlbumId" = tr."AlbumId"
+WHERE    ar."ArtistId" != ( 
+          select "ArtistId" 
+          from "Artist" arin 
+          where arin."Name" = 'Various Artists')
+GROUP BY ar."ArtistId", al."AlbumId"
+ORDER by ar."ArtistId"
 
